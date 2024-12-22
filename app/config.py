04c3d6236d545c9244c0085f23050fe1a2334f7d
Daniel Kopecky <danielkopecky@protonmail.com>
2024-12-22 21:03:30 +0300

    UPD: Readme.md

--- app/config.py
+++ app/config.py
@@ -9,7 +9,8 @@ def get_config():
              - RUNPOD_API_KEY: API key for RunPod authentication.
              - RUNPOD_ENDPOINT: API endpoint for RunPod.
              - DATASET: Path to the dataset file.
-             - LOWER_THRESHOLD: A threshold value used in the application.
+             - LOWER_THRESHOLD: A threshold value for ingredient and recipe similarity.
+             - K_NEIGHBORS: Number of neighbors to consider during index search.
     """
 
     env_vars = dotenv_values("env/.env")
@@ -20,3 +21,4 @@ def get_config():
         "LOWER_THRESHOLD": float(env_vars["LOWER_THRESHOLD"]),
         "K_NEIGHBORS": int(env_vars["K_NEIGHBORS"]),
     }
+
